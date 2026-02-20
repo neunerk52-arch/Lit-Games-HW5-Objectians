670b2680c28fe869a12d48ddc31602d18a771808
neunerk52-arch <neunerk52@gmail.com>
2026-02-20 14:08:23 -0500

    finishing HW

--- Scripts/Rotate.gd
+++ Scripts/Rotate.gd
@@ -8,6 +8,6 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	transform = transform.rotated_local(rotation_speed)
 	
